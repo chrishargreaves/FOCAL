5321d45712cbbc9930930c18f626d2f95d39a613
Chris Hargreaves <chrishargreaves@users.noreply.github.com>
2026-03-13 16:46:28 +0000

    patch for custom domain

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.BASE_PATH || '/FOCAL/',
+  base: 'BASE_PATH' in process.env ? (process.env.BASE_PATH || '/') : '/FOCAL/',
 });
